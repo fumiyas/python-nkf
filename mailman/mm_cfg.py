ee16a6cbe7edef7dc9fd4b40cd30394b410bc1d8
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-03-04 19:28:06 +0900

    Rename to nkf_codecs.py from japanesenkf.py

--- mailman/mm_cfg.py
+++ mailman/mm_cfg.py
@@ -1,7 +1,7 @@
 ## Override Japanese codecs by NKF codecs (if available) to
 ## treat invalid encoded Japanese messages from bogus MUAs.
 try:
-    import japanesenkf
-    japanesenkf.overrideEncodings()
+    import nkf_codecs
+    nkf_codecs.overrideEncodings()
 except:
     pass
--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup (name = "nkf",
        author_email="fumiyas@osstech.jp",
        url="https://github.com/fumiyas/python-nkf",
        license="BSD",
-       py_modules = ["japanesenkf"],
+       py_modules = ["nkf_codecs"],
        ext_modules = [Extension("nkf", ["nkf.c"])],
        )
