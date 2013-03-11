c1705143fcf320e9ee8aba53e39652a2f17b05c7
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-03-11 16:23:21 +0900

    mm_cfg.py: Note about mailman-no-load-iso2022jp.patch

--- mailman/mm_cfg.py
+++ mailman/mm_cfg.py
@@ -1,7 +1,9 @@
 ## Override Japanese codecs by NKF codecs (if available) to
 ## treat invalid encoded Japanese messages from bogus MUAs.
+## NOTE: mailman-no-load-iso2022jp.patch required
 try:
     import nkf_codecs
     nkf_codecs.overrideEncodings()
 except:
     pass
+
