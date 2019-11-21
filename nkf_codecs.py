f5521a04ea448d6d5c0564c976ffcd10709bb003
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2019-11-21 21:42:25 +0900

    chore: nkf_codecs.py: Change license to 2-clause BSD

--- nkf_codecs.py
+++ nkf_codecs.py
@@ -2,11 +2,34 @@
 ## -*- coding:utf-8 -*-
 ##
 ## Python 2.x: Japanese codecs by NKF
-## Copyright (c) 2007-2009 SATOH Fumiyasu @ OSS Technology, Inc.
-##               <http://www.osstech.co.jp>
+## Copyright (c) 2007-2019 SATOH Fumiyasu @ OSS Technology Corp., Japan
+##               <https://www.osstech.co.jp>
 ##
-## License: GNU General Public License version 2 or later
-## Date: 2009-05-20, since 2007-01-12
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+## 
+## 1. Redistributions of source code must retain the above copyright notice, this
+##    list of conditions and the following disclaimer.
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+## ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## 
+## The views and conclusions contained in the software and documentation are those
+## of the authors and should not be interpreted as representing official policies,
+## either expressed or implied, of the author(s).
 
 ## References:
 ##      http://gihyo.jp/dev/serial/01/pythonhacks/0004
