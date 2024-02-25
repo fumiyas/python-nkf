347fccd212180bcfb9c5ef4852f15fc52c75bf0e
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-02-25 14:23:40 +0900

    feat: nkf_codecs: Drop Python 2.5 support

--- nkf_codecs.py
+++ nkf_codecs.py
@@ -51,20 +51,15 @@ import encodings
 import sys
 import re
 
-python_version = float(re.sub(r'^(?s)(\d+\.\d+).*$', '\\1', sys.version))
-
 ## ======================================================================
 
-if python_version >= 2.5:
-    regentry_by_encoding = {}
-    def nkf_codec_search_func(encoding):
-        if regentry_by_encoding.has_key(encoding):
-            return regentry_by_encoding[encoding]
-        else:
-            return None
-    codecs.register(nkf_codec_search_func)
-else:
-    import imp
+regentry_by_encoding = {}
+def nkf_codec_search_func(encoding):
+    if regentry_by_encoding.has_key(encoding):
+        return regentry_by_encoding[encoding]
+    else:
+        return None
+codecs.register(nkf_codec_search_func)
 
 ## ======================================================================
 
@@ -108,12 +103,11 @@ class iso2022_jp_nkf_Codec(codecs.Codec):
         ret = unicode(ret, 'utf8', 'replace')
         return ret, len(input)
 
-if python_version >= 2.5:
-    class iso2022_jp_nkf_IncrementalEncoder(iso2022_jp.IncrementalEncoder):
-        codec = iso2022_jp_Codec
+class iso2022_jp_nkf_IncrementalEncoder(iso2022_jp.IncrementalEncoder):
+    codec = iso2022_jp_Codec
 
-    class iso2022_jp_nkf_IncrementalDecoder(iso2022_jp.IncrementalDecoder):
-        codec = iso2022_jp_Codec
+class iso2022_jp_nkf_IncrementalDecoder(iso2022_jp.IncrementalDecoder):
+    codec = iso2022_jp_Codec
 
 class iso2022_jp_nkf_StreamReader(iso2022_jp_nkf_Codec, codecs.StreamReader):
     def __init__(self, stream, errors='strict'):
@@ -169,27 +163,17 @@ class iso2022_jp_nkf_StreamReader(iso2022_jp_nkf_Codec, codecs.StreamReader):
 class iso2022_jp_nkf_StreamWriter(iso2022_jp_nkf_Codec, codecs.StreamWriter):
     codec = iso2022_jp_Codec
 
-if python_version >= 2.5:
-    def iso2022_jp_nkf_getregentry():
-        return codecs.CodecInfo(
-            name =                  'iso2022_jp_nkf',
-            encode =                iso2022_jp_nkf_Codec().encode,
-            decode =                iso2022_jp_nkf_Codec().decode,
-            incrementalencoder =    iso2022_jp_nkf_IncrementalEncoder,
-            incrementaldecoder =    iso2022_jp_nkf_IncrementalDecoder,
-            streamreader =          iso2022_jp_nkf_StreamReader,
-            streamwriter =          iso2022_jp_nkf_StreamWriter,
-        )
-    regentry_by_encoding['iso2022_jp_nkf'] = iso2022_jp_nkf_getregentry()
-else:
-    m = imp.new_module('iso2022_jp_nkf')
-    m.getregentry = lambda : (
-        iso2022_jp_nkf_Codec().encode,
-        iso2022_jp_nkf_Codec().decode,
-        iso2022_jp_nkf_StreamReader,
-        iso2022_jp_nkf_StreamWriter,
+def iso2022_jp_nkf_getregentry():
+    return codecs.CodecInfo(
+        name =                  'iso2022_jp_nkf',
+        encode =                iso2022_jp_nkf_Codec().encode,
+        decode =                iso2022_jp_nkf_Codec().decode,
+        incrementalencoder =    iso2022_jp_nkf_IncrementalEncoder,
+        incrementaldecoder =    iso2022_jp_nkf_IncrementalDecoder,
+        streamreader =          iso2022_jp_nkf_StreamReader,
+        streamwriter =          iso2022_jp_nkf_StreamWriter,
     )
-    sys.modules['iso2022_jp_nkf'] = m
+regentry_by_encoding['iso2022_jp_nkf'] = iso2022_jp_nkf_getregentry()
 
 ## EUC-JP codecs by NKF
 ## ----------------------------------------------------------------------
@@ -209,12 +193,11 @@ class euc_jp_nkf_Codec(codecs.Codec):
         ret = unicode(ret, 'utf8', 'replace')
         return ret, len(input)
 
-if python_version >= 2.5:
-    class euc_jp_nkf_IncrementalEncoder(euc_jp.IncrementalEncoder):
-        codec = euc_jp_Codec
+class euc_jp_nkf_IncrementalEncoder(euc_jp.IncrementalEncoder):
+    codec = euc_jp_Codec
 
-    class euc_jp_nkf_IncrementalDecoder(euc_jp.IncrementalDecoder):
-        codec = euc_jp_Codec
+class euc_jp_nkf_IncrementalDecoder(euc_jp.IncrementalDecoder):
+    codec = euc_jp_Codec
 
 class euc_jp_nkf_StreamReader(euc_jp_nkf_Codec, codecs.StreamReader):
     def __init__(self, stream, errors='strict'):
@@ -262,27 +245,17 @@ class euc_jp_nkf_StreamReader(euc_jp_nkf_Codec, codecs.StreamReader):
 class euc_jp_nkf_StreamWriter(euc_jp_nkf_Codec, codecs.StreamWriter):
     codec = euc_jp_Codec
 
-if python_version >= 2.5:
-    def euc_jp_nkf_getregentry():
-        return codecs.CodecInfo(
-            name =                  'euc_jp_nkf',
-            encode =                euc_jp_nkf_Codec().encode,
-            decode =                euc_jp_nkf_Codec().decode,
-            incrementalencoder =    euc_jp_nkf_IncrementalEncoder,
-            incrementaldecoder =    euc_jp_nkf_IncrementalDecoder,
-            streamreader =          euc_jp_nkf_StreamReader,
-            streamwriter =          euc_jp_nkf_StreamWriter,
-        )
-    regentry_by_encoding['euc_jp_nkf'] = euc_jp_nkf_getregentry()
-else:
-    m = imp.new_module('euc_jp_nkf')
-    m.getregentry = lambda : (
-        euc_jp_nkf_Codec().encode,
-        euc_jp_nkf_Codec().decode,
-        euc_jp_nkf_StreamReader,
-        euc_jp_nkf_StreamWriter,
+def euc_jp_nkf_getregentry():
+    return codecs.CodecInfo(
+        name =                  'euc_jp_nkf',
+        encode =                euc_jp_nkf_Codec().encode,
+        decode =                euc_jp_nkf_Codec().decode,
+        incrementalencoder =    euc_jp_nkf_IncrementalEncoder,
+        incrementaldecoder =    euc_jp_nkf_IncrementalDecoder,
+        streamreader =          euc_jp_nkf_StreamReader,
+        streamwriter =          euc_jp_nkf_StreamWriter,
     )
-    sys.modules['euc_jp_nkf'] = m
+regentry_by_encoding['euc_jp_nkf'] = euc_jp_nkf_getregentry()
 
 ## Shift_JIS codecs by NKF
 ## ----------------------------------------------------------------------
@@ -302,12 +275,11 @@ class shift_jis_nkf_Codec(codecs.Codec):
         ret = unicode(ret, 'utf8', 'replace')
         return ret, len(input)
 
-if python_version >= 2.5:
-    class shift_jis_nkf_IncrementalEncoder(shift_jis.IncrementalEncoder):
-        codec = shift_jis_Codec
+class shift_jis_nkf_IncrementalEncoder(shift_jis.IncrementalEncoder):
+    codec = shift_jis_Codec
 
-    class shift_jis_nkf_IncrementalDecoder(shift_jis.IncrementalDecoder):
-        codec = shift_jis_Codec
+class shift_jis_nkf_IncrementalDecoder(shift_jis.IncrementalDecoder):
+    codec = shift_jis_Codec
 
 class shift_jis_nkf_StreamReader(shift_jis_nkf_Codec, codecs.StreamReader):
     def __init__(self, stream, errors='strict'):
@@ -355,87 +327,54 @@ class shift_jis_nkf_StreamReader(shift_jis_nkf_Codec, codecs.StreamReader):
 class shift_jis_nkf_StreamWriter(shift_jis_nkf_Codec, codecs.StreamWriter):
     codec = shift_jis_Codec
 
-if python_version >= 2.5:
-    def shift_jis_nkf_getregentry():
-        return codecs.CodecInfo(
-            name =                  'shift_jis_nkf',
-            encode =                shift_jis_nkf_Codec().encode,
-            decode =                shift_jis_nkf_Codec().decode,
-            incrementalencoder =    shift_jis_nkf_IncrementalEncoder,
-            incrementaldecoder =    shift_jis_nkf_IncrementalDecoder,
-            streamreader =          shift_jis_nkf_StreamReader,
-            streamwriter =          shift_jis_nkf_StreamWriter,
-        )
-    regentry_by_encoding['shift_jis_nkf'] = shift_jis_nkf_getregentry()
-else:
-    m = imp.new_module('shift_jis_nkf')
-    m.getregentry = lambda : (
-        shift_jis_nkf_Codec().encode,
-        shift_jis_nkf_Codec().decode,
-        shift_jis_nkf_StreamReader,
-        shift_jis_nkf_StreamWriter,
+def shift_jis_nkf_getregentry():
+    return codecs.CodecInfo(
+        name =                  'shift_jis_nkf',
+        encode =                shift_jis_nkf_Codec().encode,
+        decode =                shift_jis_nkf_Codec().decode,
+        incrementalencoder =    shift_jis_nkf_IncrementalEncoder,
+        incrementaldecoder =    shift_jis_nkf_IncrementalDecoder,
+        streamreader =          shift_jis_nkf_StreamReader,
+        streamwriter =          shift_jis_nkf_StreamWriter,
     )
-    sys.modules['shift_jis_nkf'] = m
+regentry_by_encoding['shift_jis_nkf'] = shift_jis_nkf_getregentry()
 
 ## ----------------------------------------------------------------------
 
 ## Override Japanese codecs by *_nkf
 
 def overrideEncodings():
-    if python_version >= 2.5:
-        encodings._cache['iso2022_jp'] = iso2022_jp_nkf_getregentry()
-        encodings._cache['euc_jp'] = euc_jp_nkf_getregentry()
-        encodings._cache['shift_jis'] = shift_jis_nkf_getregentry()
+    encodings._cache['iso2022_jp'] = iso2022_jp_nkf_getregentry()
+    encodings._cache['euc_jp'] = euc_jp_nkf_getregentry()
+    encodings._cache['shift_jis'] = shift_jis_nkf_getregentry()
 
-        ## 'iso2022_jp' and 'iso2022-jp' are not in aliases
-        encodings._cache['iso2022-jp'] = iso2022_jp_nkf_getregentry()
-        ## 'euc_jp' and 'euc-jp' are not in aliases
-        encodings._cache['euc-jp'] = euc_jp_nkf_getregentry()
-        ## 'euc_jp' and 'euc-jp' are not in aliases
-        encodings._cache['shift-jis'] = shift_jis_nkf_getregentry()
+    ## 'iso2022_jp' and 'iso2022-jp' are not in aliases
+    encodings._cache['iso2022-jp'] = iso2022_jp_nkf_getregentry()
+    ## 'euc_jp' and 'euc-jp' are not in aliases
+    encodings._cache['euc-jp'] = euc_jp_nkf_getregentry()
+    ## 'euc_jp' and 'euc-jp' are not in aliases
+    encodings._cache['shift-jis'] = shift_jis_nkf_getregentry()
 
-        ## Override aliases for Japanese codecs by *_nkf
-        for alias in encodings.aliases.aliases:
-            encoder = None
-            if re.match(r'^iso_?2022_?jp(_?(1|ext))?$', alias):
-                encoder = iso2022_jp_nkf_getregentry()
-            elif re.match(r'^(euc_?jp|u_?jis)$', alias):
-                encoder = euc_jp_nkf_getregentry()
-            elif re.match(r'^s(hift)?_?jis$', alias):
-                encoder = shift_jis_nkf_getregentry()
-            if encoder:
-                encodings._cache[alias] = encoder
-                alias2 = re.sub('_', '-', alias)
-                if alias2 != alias:
-                    encodings._cache[alias2] = encoder
-    else:
-        encodings.aliases.aliases.update({
-            'iso2022_jp':	'iso2022_jp_nkf',
-            'euc_jp':		'euc_jp_nkf',
-            'shift_jis':	'shift_jis_nkf',
-        })
-
-        encodings.aliases.aliases.update({'iso2022jp': 'iso2022_jp_nkf'})
-
-        ## Override aliases for Japanese codecs by *_nkf
-        for alias in encodings.aliases.aliases:
-            encoding = None
-            if re.match(r'^iso_?2022_?jp(_?(1|ext))?$', alias):
-                encoding = 'iso2022_jp_nkf'
-            elif re.match(r'^(euc_?jp|u_?jis)$', alias):
-                encoding = 'euc_jp_nkf'
-            elif re.match(r'^s(hift)?_?jis$', alias):
-                encoding = 'shift_jis_nkf'
-            if encoding:
-                encodings.aliases.aliases.update({alias: encoding})
-
-        encodings._cache.clear()
+    ## Override aliases for Japanese codecs by *_nkf
+    for alias in encodings.aliases.aliases:
+        encoder = None
+        if re.match(r'^iso_?2022_?jp(_?(1|ext))?$', alias):
+            encoder = iso2022_jp_nkf_getregentry()
+        elif re.match(r'^(euc_?jp|u_?jis)$', alias):
+            encoder = euc_jp_nkf_getregentry()
+        elif re.match(r'^s(hift)?_?jis$', alias):
+            encoder = shift_jis_nkf_getregentry()
+        if encoder:
+            encodings._cache[alias] = encoder
+            alias2 = re.sub('_', '-', alias)
+            if alias2 != alias:
+                encodings._cache[alias2] = encoder
 
 ## Test
 ## ======================================================================
 
 if __name__ == '__main__':
-    import StringIO
+    from io import StringIO
 
     unicoded =  u'abc ABC'
     unicoded += u' 阿異雨あいう'
