eba494f9820f8bf3ab6d341e33974435693d810a
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-02-27 10:59:21 +0900

    chore: Migrate to setuptools from distutils.core

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 setup(
-    name = "nkf",
+    name="nkf",
     version="1.0.0",
     description="Python Interface to NKF",
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author="SATOH Fumiyasu",
     author_email="fumiyas@osstech.co.jp",
     url="https://github.com/fumiyas/python-nkf",
     license="BSD",
-    py_modules = ["nkf_codecs"],
-    ext_modules = [Extension("nkf", ["nkf.c"])],
-    )
+    py_modules=["nkf_codecs"],
+    ext_modules=[Extension("nkf", ["nkf.c"])],
+)
