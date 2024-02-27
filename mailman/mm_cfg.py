9ca1a9f388d640f463f5cd8f8276e8d9531a53ae
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2024-02-27 23:12:27 +0900

    feat: Remove hacks for Mailman 2

--- mailman/mm_cfg.py
+++
@@ -1,9 +0,0 @@
-## Override Japanese codecs by NKF codecs (if available) to
-## treat invalid encoded Japanese messages from bogus MUAs.
-## NOTE: mailman-no-load-iso2022jp.patch required
-try:
-    import nkf_codecs
-    nkf_codecs.overrideEncodings()
-except:
-    pass
-
