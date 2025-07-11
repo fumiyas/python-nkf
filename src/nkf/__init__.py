e00f00d39c48a942e5ef85544f4f9fcd69d327f7
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2025-07-11 13:46:47 +0900

    refactor: Import nkf._* from the relative path

--- src/nkf/__init__.py
+++ src/nkf/__init__.py
@@ -1,6 +1,6 @@
 ## -*- coding: utf-8 -*- vim:shiftwidth=4:expandtab:
 
-from nkf._version import __version__
-from nkf._nkf import *
+from ._version import __version__
+from ._nkf import *
 
 VERSION = __version__
